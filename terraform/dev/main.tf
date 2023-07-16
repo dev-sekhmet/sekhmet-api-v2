77a681090f9ea84daf148850e3d8ffd2a80435b5
Gaetan TEMATE <gaetan@temate.fr>
2023-07-16 09:55:05 +0200

    dev stable 11

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,55 +1,55 @@
 # Create Application Load Balancer
 module "alb" {
-  source          = "../modules/alb"
-  vpc_id          = var.vpc_id
-  alb_name        = var.alb_name
-  target_port     = var.target_port
-  listener_port   = var.listener_port
-  security_groups = var.alb_security_groups
-  subnets         = var.subnets
+  source            = "../modules/alb"
+  vpc_id            = var.vpc_id
+  alb_name          = var.alb_name
+  target_port       = var.target_port
+  listener_port     = var.listener_port
+  security_groups   = var.alb_security_groups
+  subnets           = var.subnets
   target_group_name = var.target_group_name
-  application_env = "Dev"
+  application_env   = "Dev"
 }
 
 # Create Auto Scaling Group
 module "asg" {
-  source           = "../modules/asg"
-  vpc_id           = var.vpc_id
-  asg_name         = var.asg_name
-  min_instances    = var.min_instances
-  max_instances    = var.max_instances
-  desired_capacity = var.desired_capacity
-  target_group_arn = module.alb.target_group_arn
-  scale_out_cpu    = var.scale_out_cpu
-  scale_in_cpu     = var.scale_in_cpu
-  ami_id           = var.ami_id
-  user_data = var.user_data
-  codeartefact_domain_name = var.codeartefact_domain_name
+  source                    = "../modules/asg"
+  vpc_id                    = var.vpc_id
+  asg_name                  = var.asg_name
+  min_instances             = var.min_instances
+  max_instances             = var.max_instances
+  desired_capacity          = var.desired_capacity
+  target_group_arn          = module.alb.target_group_arn
+  scale_out_cpu             = var.scale_out_cpu
+  scale_in_cpu              = var.scale_in_cpu
+  ami_id                    = var.ami_id
+  user_data                 = var.user_data
+  codeartefact_domain_name  = var.codeartefact_domain_name
   codeartefact_domain_owner = var.codeartefact_domain_owner
-  instance_profile = var.instance_profile
-  instance_type = var.instance_type
-  route53_cname_name = var.route53_cname_name
-  route53_zone_id = var.route53_zone_id
-  subnets = var.subnets
-  key_name = var.key_name
-  application_version = var.application_version
-  application_env = "Dev"
-  twilio_conversation_sid = var.twilio_conversation_sid
-  twilio_account_sid = var.twilio_account_sid
-  twilio_auth_token = var.twilio_auth_token
-  twilio_api_secret = var.twilio_api_secret
-  twilio_verify_sid = var.twilio_verify_sid
-  dynamodb_endpoint = var.dynamodb_endpoint
-  aws_region = var.aws_region
+  instance_profile          = var.instance_profile
+  instance_type             = var.instance_type
+  route53_cname_name        = var.route53_cname_name
+  route53_zone_id           = var.route53_zone_id
+  subnets                   = var.subnets
+  key_name                  = var.key_name
+  application_version       = var.application_version
+  application_env           = "Dev"
+  twilio_conversation_sid   = var.twilio_conversation_sid
+  twilio_account_sid        = var.twilio_account_sid
+  twilio_auth_token         = var.twilio_auth_token
+  twilio_api_secret         = var.twilio_api_secret
+  twilio_verify_sid         = var.twilio_verify_sid
+  dynamodb_endpoint         = var.dynamodb_endpoint
+  aws_region                = var.aws_region
 }
 
 # Create DynamoDB table
 module "dynamodb" {
-  source               = "../modules/dynamodb"
+  source                        = "../modules/dynamodb"
   dynamodb_capacity_mode        = var.dynamodb_capacity_mode
   dynamodb_read_capacity_units  = var.dynamodb_read_capacity_units
   dynamodb_write_capacity_units = var.dynamodb_write_capacity_units
-  application_env = "Dev"
+  application_env               = "Dev"
 }
 
 # Route53 CNAME record
